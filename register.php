5be3e205324f1daca2a1a9be2e8916dd702bb330
Nikhil Nagdev <nikhil.nagdev@outlook.com>
2019-03-17 14:34:48 +0530

    Creted new pages

--- register.php
+++ register.php
@@ -32,23 +32,25 @@
           <div class="col-lg-7">
             <div class="p-5">
               <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                <h1 class="h4 text-gray-900 mb-4">Create an User!</h1>
               </div>
+
               <form class="user">
-                <div class="form-group row">
-                  <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                  <div class="form-group">
+                      <input type="text" class="form-control form-control-user" id="name" placeholder="Name">
                   </div>
-                  <div class="col-sm-6">
-                    <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
-                  </div>
-                </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                  <input name="user_email" type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
                 </div>
+                  <div class="form-group">
+                      <input name="user_contact" type="number" class="form-control form-control-user" id="user_contact" placeholder="Contact no">
+                  </div>
+                  <div class="form-group">
+                      <textarea name="user_address" id="user_address" placeholder="Address" class="form-control"></textarea>
+                  </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                    <input name="password" type="password" class="form-control form-control-user" id="password" placeholder="Password">
                   </div>
                   <div class="col-sm-6">
                     <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
@@ -58,13 +60,8 @@
                   Register Account
                 </a>
                 <hr>
-                <a href="index.php" class="btn btn-google btn-user btn-block">
-                  <i class="fab fa-google fa-fw"></i> Register with Google
-                </a>
-                <a href="index.php" class="btn btn-facebook btn-user btn-block">
-                  <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                </a>
               </form>
+
               <hr>
               <div class="text-center">
                 <a class="small" href="forgot-password.html">Forgot Password?</a>
