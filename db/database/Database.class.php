3397f9d46db5aa6b213ae07d032eaa1760cd387b
Shailesh Jain <jainshailesh91@gmail.com>
2019-03-21 14:38:39 +0530

    Started database coding.

--- db/database/Database.class.php
+++ db/database/Database.class.php
@@ -7,12 +7,14 @@
  */
 
 require_once 'constants.php';
+require_once '../utils/functions.php';
 
 class Database
 {
     private static $pdo;
     private static $dsn = "mysql:host=".HOST.";dbname=".DB;
     private static $isInitialized = false;
+    private static $associativeArray;
 
     /**
      * This method is the database initialization method. This method must be called before calling any other method of this class.
@@ -34,29 +36,70 @@ class Database
 
     /**
      * This method checks if the init() method was called or not before calling any other method.
-     * @return bool
+     * @return bool returns true if the init() method was called as the first method.
      */
     private static function isInitialized()
     {
         if(!self::$isInitialized)
             throw new BadMethodCallException("init() method must be called first");
         return true;
-
     }
-    public static function insert($tableName, $associativeArray)
+
+    /**
+     * Checks if the table exists or not in the database.
+     * @param $tableName - name of the table to check if it exists or not.
+     * @return bool - returns true if exists else false.
+     */
+    private static function tableExists($tableName)
     {
         if(self::isInitialized())
         {
-            $sql = "INSERT INTO {$tableName} ";
-            $pdoStmt = self::$pdo->prepare($sql);
+            $query = "SHOW TABLES FROM ".DB." LIKE '{$tableName}'";
+            $tablesInDB = self::$pdo->query($query);
+            if($tablesInDB)
+            {
+                if($tablesInDB->rowCount() == 1){
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
+    /**
+     * this method inserts into table specified with the respective columns and values provided in the associativeArray
+     * @param $tableName - table in which to insert
+     * @param $associativeArray - array of columns and values to be inserted
+     * @return bool - returns true if insertion was successful
+     */
+    public static function insert($tableName, $associativeArray)
+    {
+        if(self::isInitialized() && self::tableExists($tableName))
+        {
+            $columnStatement = getString($associativeArray, INSERT_QUERY_FORMAT);
+            $query = "INSERT INTO {$tableName}{$columnStatement}";
+            $pdoStmt = self::$pdo->prepare($query);
+            $keys = array_keys($associativeArray);
+            for($i = 0; $i<count($associativeArray); $i++)
+                $pdoStmt->bindParam($i+1, $associativeArray[$keys[$i]]);
+            if($pdoStmt->execute())
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * updates the table specified with the respective values provided in the associativeArray
+     * @param $tableName - table to update
+     * @param $associativeArray - array of columns and values to be updated
+     */
     public static function update($tableName, $associativeArray)
     {
-        if (self::isInitialized())
+        if (self::isInitialized() && self::tableExists($tableName))
         {
-
+            $columnStatement = getString($associativeArray, UPDATE_QUERY_FORMAT);
+            $query = "UPDATE {$tableName} SET {$columnStatement}";
+            die($query);
         }
     }
 
@@ -64,7 +107,11 @@ class Database
     {
         if(self::isInitialized())
         {
-            $sql = "";
+            $query = "";
         }
     }
 }
+
+Database::init();
+//echo Database::insert('products', array("product_name"=>"Gold Ring", "product_quantity"=>"100"));
+Database::update('products', array("product_name"=>"Gold Ring", "product_quantity"=>"100"));
--- db/utils/functions.php
+++ db/utils/functions.php
@@ -1,7 +1,35 @@
 <?php
+require_once '../database/constants.php';
+
 /**
- * Created by PhpStorm.
- * User: Shailu
- * Date: 21-03-2019
- * Time: 12:54 AM
+ * THis function is basically a utility function which is used to convert associativeArray
+ * into the query format used for insertion and updating. (in prepared statement format)
+ * @param $associativeArray - array to be converted
+ * @param $constant - to specify the format.
+ * @return string - return the columns and values in insert or update query format
  */
+function getString($associativeArray, $constant){
+    $numKeys = count($associativeArray);
+    $keys = array_keys($associativeArray);
+    if($constant==INSERT_QUERY_FORMAT){
+        $string="(";
+        $string1="(";
+        for($i=0; $i < $numKeys; ++$i)
+        {
+            $string.="$keys[$i],";
+            $string1.="?,";
+        }
+        $string[strlen($string)-1]=")";
+        $string1[strlen($string1)-1]=")";
+        $string.=" VALUES ".$string1;
+    }
+    else if($constant==UPDATE_QUERY_FORMAT){
+        $string="";
+        for($i=0; $i < $numKeys; ++$i) {
+            $string.="$keys[$i] = ?";
+            if($i!=$numKeys-1)
+                $string.=",";
+        }
+    }
+    return $string;
+}
