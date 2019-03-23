67b7d073b6c3a1c9e938093af1fd616e53927edc
Shailesh Jain <jainshailesh91@gmail.com>
2019-03-24 00:18:20 +0530

    Changes in ui files

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(function(){
-    $('a.ajax-link').click(function(event){
-        page = $(event.currentTarget).attr('data-path');
-        $.ajax({
-            url: "http://localhost/JewelleryMS/php/ajax-handler.php?src="+ page,
-            success: function(result){
-                $("#include").html(result);
-            }
-        });
-    });
+    // $('a.ajax-link').click(function(event){
+    //     page = $(event.currentTarget).attr('data-path');
+    //     $.ajax({
+    //         url: "http://localhost/JewelleryMS/php/ajax-handler.php?src="+ page,
+    //         success: function(result){
+    //             $("#include").html(result);
+    //         }
+    //     });
+    // });
 });
--- vendor/jquery/jquery.slim.js
+++ vendor/jquery/jquery.slim.js
@@ -4540,7 +4540,7 @@ function adjustCSS( elem, prop, valueParts, tween ) {
 		while ( maxIterations-- ) {
 
 			// Evaluate and update our best guess (doubling guesses that zero out).
-			// Finish if the scale equals or crosses 1 (making the old*new product non-positive).
+			// Finish if the scale equals or crosses 1 (making the old*new products non-positive).
 			jQuery.style( elem, prop, initialInUnit + unit );
 			if ( ( 1 - scale ) * ( 1 - ( scale = currentValue() / initial || 0.5 ) ) <= 0 ) {
 				maxIterations = 0;
