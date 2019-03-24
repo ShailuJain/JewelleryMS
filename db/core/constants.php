e0cce9b9a9c9fc4522b738b0d705fdace1686946
sanjay rohra <rohra460@gmail.com>
2019-03-24 12:35:55 +0530

    add-category updated

--- db/core/constants.php
+++ db/core/constants.php
@@ -8,7 +8,7 @@
 //Database related constants
 define("HOST","localhost");
 define("DB","jms");
-define("USERNAME","jainshailesh91");
-define("PASSWORD","shailujain123");
+define("USERNAME","sanjay");
+define("PASSWORD","sanjay");
 define("INSERT_QUERY_FORMAT",1);
 define("UPDATE_QUERY_FORMAT",2);
--- includes/pages/categories/add-category.php
+++ includes/pages/categories/add-category.php
@@ -1,27 +1,66 @@
+<?php
+    require_once('db/models/Category.class.php');
+    require_once('db/models/GST.class.php');
+    if(isset($_POST['add_category'])){
+        try{
+            CRUD::init();
+            CRUD::query("SET AUTOCOMMIT=OFF");
+            if(!empty($_POST['category_name']) && !empty($_POST['hsn_code']) && !empty($_POST['gst_rate'])){
+                echo"HIII";
+                $category = new category();
+                $GST = new GST();
+                $category->category_name = $_POST['category_name'];
+                $category->hsn_code = $GST->hsn_code = $_POST['hsn_code'];
+                $GST->gst_rate = $_POST['gst_rate'];
+//                $result = CRUD::select("gst","*", "hsn_code = $GST->hsn_code");
+//                $row = $result->fetch();
+                $result1 = CRUD::select("categories","*","category_name = '$category->category_name'");
+                $row1 = $result1->fetch();
+                if(!$row1){
+                    if($category->insert() and $GST->insert()){
+                        crud::query("COMMIT");
+                    }
+                    else{
+                        crud::query("ROLLBACK");
+                        echo"ERROR INSERTING";
+                        //UI FOR ERRORS WHILE INSERTING
+                    }
+                }
+                else{
+                    echo"HSN ALREADY EXIST";
+                    //UI FOR HSN ALREADY EXISTING
+                }
+            }
+            else{
+                echo"PLEASE ENTER ALL DETAILS PROPERLY";
+                //UI FOR ALL FIELDS
+            }
+        }
+        catch(Exception $e){
+            crud::query("ROLLBACK");
+        }
+        crud::select("ROLLBACK");
+    }
+?>
 <div class="row">
     <div class="offset-1 col-md-10">
-        <form action="" method="" role="form" enctype="multipart/form-data">
+        <form action="" method="POST" role="form" enctype="multipart/form-data">
             <legend>Add New Category</legend>
             <hr>
 
             <div class="form-group">
-                <label for="cat_name">Category Name</label>
-                <input type="text" class="form-control" name="cat_name" id="cat_name" placeholder="Enter category name">
+                <label for="category_name">Category Name</label>
+                <input type="text" class="form-control" name="category_name" id="category_name" placeholder="Enter category name">
             </div>
 
             <div class="form-group">
-                <label for="cat_hsn">Hsn Code</label>
-                <input type="text" class="form-control" name="cat_hsn" id="cat_hsn" placeholder="Enter hsn-code">
+                <label for="hsn_code">Hsn Code</label>
+                <input type="number" class="form-control" name="hsn_code" id="hsn_code" placeholder="Enter hsn-code">
             </div>
 
             <div class="form-group">
-                <label for="quantity">Quantity</label>
-                <input type="number" class="form-control" name="cat_quantity" id="cat_quantity" placeholder="Enter quantity">
-            </div>
-
-            <div class="form-group">
-                <label for="cat_gst_rate">GST Rate</label>
-                <input type="number" class="form-control" name="cat_gst_rate" id="cat_gst_rate" placeholder="Enter GST rate">
+                <label for="gst_rate">GST Rate</label>
+                <input type="number" class="form-control" name="gst_rate" id="gst_rate" placeholder="Enter GST rate">
             </div>
             <button type="submit" name="add_category" id="add_category" class="btn btn-primary">Add Category</button>
         </form>
