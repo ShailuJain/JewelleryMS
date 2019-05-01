2f37a13ccf7fcaf93ac0ab5e5d9c361065cfb308
NikhilNagdev <nnagdev58@gmail.com>
2019-05-01 14:34:53 +0530

    Changes in purchase

---
+++ includes/pages/purchases/view-all-purchases.php
@@ -0,0 +1,59 @@
+<?php
+$model_name = "Purchase";
+require_once "db/models/{$model_name}.class.php";
+$rs = Purchase::viewAll();
+//This array will store the table headers for the columns we are selecting from databse
+$column_names_as = array(
+    "purchase_description" => "Description",
+    "date_of_purchase" => "Date of purchase",
+    "total_purchase_amount" => "Total amount",
+    "supplier_name" => "Supplier"
+);
+?>
+    <div class="row">
+        <div class="offset-1 col-md-10">
+            <div class="table-responsive">
+                <table id="tables" class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <?php
+                        foreach ($column_names_as as $column_name_as) {
+                            echo "<th>{$column_name_as}</th>";
+                        }
+                        ?>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                        <th>View-details</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $column_names = array_keys($column_names_as);
+                    while($row = $rs->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<tr>";
+                        foreach ($column_names as $column_name) {
+                            if(empty($row[$column_name]))
+                            {
+                                echo "<td>NULL</td>";
+                            }
+                            else
+                            {
+                                echo "<td>$row[$column_name]</td>";
+                            }
+                        }
+                        echo "<td><a class='btn btn-primary text-white' data-toggle='tooltip' href='' data-html='true' title='Edit'><i class='fa fa-edit'></i></a></td>";
+                        echo "<td><a class='btn btn-danger text-white'  data-toggle='tooltip' data-html='true' title='Delete'  data-delete=''><i class='fa fa-times'></i></a></td>";
+                        echo "</tr>";
+                        echo "<td><a href=''>view-all</a></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+<?php
+include_once 'includes/modal.php';
+
+?>
