3e39ed2795a4b75d07fe4319580300f030da0d6e
ShailuJain <jainshailesh91@gmail.com>
2024-10-19 12:02:46 +0530

    dont add new page for invoice

--- includes/pages/invoices/view-invoice.php
+++ includes/pages/invoices/view-invoice.php
@@ -46,7 +46,7 @@ try{
 
             $inv_temp = new InvoiceTemplate("Invoice - $customer->customer_name", $shop , $customer, $invoice_to_create, $products);
 //echo $inv_temp->createAndReturn();
-            $inv_temp->createAndRedirect();
+            $inv_temp->build();
         }else{
             throw new Exception("Cannot view invoice. Something went wrong");
         }
@@ -57,3 +57,130 @@ try{
     redirect_to(VIEW_ALL_INVOICES);
     setStatusAndMsg("error",$ex->getMessage());
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $inv_temp->title; ?></title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'>
+    <link href='css/style.css' rel='stylesheet'>
+    <link rel='stylesheet' href='css/invoice-template.css'>
+</head>
+<body>
+    <div class='container-fluid p-5'>
+        <div class="buttons text-center">
+            <div class="btn btn-primary print-hidden" id="printBtn">PRINT</div>
+        </div>
+        <div class='label-bold'>GST Invoice</div>
+        <div class='invoice'>
+            <div class='container-fluid shop-details p-0 mb-5'>
+                <div class="detail-header p-2 b-bottom">
+                    <div class="bold">Invoice No. : <?php echo $invoice_to_create->invoice_id; ?></div>
+                    <div class="bold">Date : <?php echo $invoice_to_create->invoice_date; ?></div>
+                </div>
+                <div class="shop-detailed b-bottom p-2 text-center">
+                    <h5 class='shop-name'><?php echo $shop->shop_name; ?></h5>
+                    <h6 class='shop-address'>Address: <?php echo $shop->shop_address; ?></h6>
+                    <h6 class='shop-address'>Mob no.: <?php echo $shop->shop_contact; ?></h6>
+                    <h6 class="bold">GSTIN: <?php echo $shop->shop_gst_no; ?> | PAN NO: <?php echo $shop->pan_no; ?></h6>
+                </div>
+                <div class='customer-details p-2'>
+                    <div class="billed-to bold">Billed To:</div>
+                    <div class='customer-name bold'><?php echo $customer->customer_name; ?></div>
+                    <div class='customer-address'>Address: <?php echo $customer->customer_address; ?></div>
+                    <div class='customer-address'>Contact: <?php echo $customer->customer_contact; ?></div>
+                </div>
+            </div>
+            <div class="invoice-body bord">
+                <div class="invoice-main-body">
+                    <div class="invoice-body-heading">
+                        <div class="columns">
+                            <div class="row m-0">
+                                <div class="b-right col-md-4"></div>
+                                <div class="b-right col-md-1"></div>
+                                <div class="b-right col-md-1"></div>
+                                <div class="b-right col-md-2"></div>
+                                <div class="b-right col-md-1"></div>
+                                <div class="b-right col-md-1"></div>
+                                <div class="col-md-2"></div>
+                            </div>
+                        </div>
+                        <div class="row b-bottom m-0 headings">
+                            <span class="col-md-4 heading font-weight-bold">Particulars</span>
+                            <span class="col-md-1 heading font-weight-bold">Net Wt.<br>(In Grams)</span>
+                            <span class="col-md-1 heading font-weight-bold">Rate/gm</span>
+                            <span class="col-md-2 heading font-weight-bold">Taxable Value</span>
+                            <span class="col-md-1 heading font-weight-bold heading-gst">CGST<br>(Amt & Rate)</span>
+                            <span class="col-md-1 heading font-weight-bold heading-gst">SGST<br>(Amt & Rate)</span>
+                            <span class="col-md-2 heading font-weight-bold">Amount with GST</span>
+                        </div>
+                    </div>
+                    <div class="invoice-body-contents">
+                        <?php foreach ($products as $product) { ?>
+                            <div class="row m-0">
+                                <div class="col-md-4"><?php echo $product->product_name; ?></div>
+                                <div class="col-md-1"><?php echo $product->net_weight; ?></div>
+                                <div class="col-md-1"><?php echo $product->rate_per_gram; ?></div>
+                                <div class="col-md-2"><?php echo $product->taxable_value; ?></div>
+                                <div class="col-md-1"><?php echo $product->cgst_amount; ?> (<?php echo $product->cgst_rate; ?>%)</div>
+                                <div class="col-md-1"><?php echo $product->sgst_amount; ?> (<?php echo $product->sgst_rate; ?>%)</div>
+                                <div class="col-md-2"><?php echo $product->total_amount_with_gst; ?></div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+                <div class="invoice-body-footer b-top row m-0 pr-0">
+                    <div class="col-md-6 bold">Total : </div>
+                    <div class="col-md-2 b-left total-amount">
+                        <span class="text-14 total-amount-content">&#8377;<?php echo $invoice_to_create->total_amount; ?></span>
+                    </div>
+                    <div class="b-left text-14 col-md-1">&#8377;<?php echo $invoice_to_create->total_cgst; ?></div>
+                    <div class="b-left text-14 col-md-1">&#8377;<?php echo $invoice_to_create->total_sgst; ?></div>
+                    <div class="gst col-md-2 b-left">
+                        <div class="text-14">GRAND TOTAL : &#8377;<?php echo $invoice_to_create->grand_total_with_gst; ?></div>
+                    </div>
+                </div>
+                <div class="amount-in-words b-top row m-0">
+                    <span class="col-md-12 bold">Amount in words: <?php echo $invoice_to_create->amount_in_words; ?></span>
+                </div>
+            </div>
+           <br>
+           <br>
+           <div class="main-footer row m-0">
+               <div class="received-goods col-md-5">
+                   <span>RECEIVED ABOVE GOODS E. AND O.E</span>
+                   <br>
+                   <br>
+                   <span>NOTE:- NO E-WAY BILL IS REQUIRED TO BE GENERATED AS THE GOODS COVERED UNDER THIS INVOICE ARE EXEMPTED AS PER SERIAL NO. 4/5 TO THE ANNEXURE TO RULE 138 (14) OF THE CGST RULES OF 2017</span>
+                </div>
+                <div class="col-md-2 offset-1 bord customer-sign-body p-0">
+                        <div class="customer-sign-content b-top">
+                            CUSTOMER SIGN
+                        </div>
+                </div>
+                <div class="col-md-4 b-top b-right b-bottom p-0 for-shop">
+                            <div class="for-shop-heading text-center b-bottom text-uppercase">
+                                FOR <?php echo $shop->shop_name; ?>
+                            </div>
+                            <div class="for-shop-content text-center b-top">
+                                <div class="for-shop-main-content">
+                                    PROPRIETOR/AUTH.SIGN
+                                </div>
+                            </div>
+                </div>
+            </div>
+            <div class="offset-8 text-center col-md-4 b-bottom b-right b-left">THANKS</div>
+            <br>
+            <br>
+            <div class="final-footer row m-0">
+                <div class="col-md-12 text-center bord p-0">
+                    <div class="b-bottom">SUBJECT TO MUMBAI JURISDICTION</div>
+                    <div>THIS IS A COMPUTER GENERATED INVOICE</div>
+                </div>
+            </div>
+    </div>
+    <script src='vendor/jquery/jquery.min.js'></script>
+    <script src='vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
+    <script src='js/invoices/print-invoice.js'></script>
+</body>
+</html>
--- invoices.php
+++ invoices.php
@@ -4,9 +4,9 @@ if (isset($_GET['src'])) {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
     }
-    $include_page = "pages/invoices/".$src.".php";
+    @include_once ("includes/pages/invoices/".$src.".php");
 } else {
     $include_page = "pages/invoices/view-all-invoices.php";
+    $title = "Invoice";
+    require_once('helpers/static-components.php');
 }
-$title = "Invoice";
-require_once('helpers/static-components.php');
